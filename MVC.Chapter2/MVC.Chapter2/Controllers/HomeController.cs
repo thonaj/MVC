7da2caf739ff0e667f23d3279f8418b424842ebd
jason goodell <jason.goodell@outlook.com>
2016-11-07 08:09:58 -0500

    add controller

--- MVC.Chapter2/MVC.Chapter2/Controllers/HomeController.cs
+++ MVC.Chapter2/MVC.Chapter2/Controllers/HomeController.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.Chapter2.Models;
 
 namespace MVC.Chapter2.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ViewResult Index()
-        {
+      // GET: Home
+      public ViewResult Index()
+      {
          int hour = DateTime.Now.Hour;
          ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
          return View();
-        }
+      }
+      [HttpGet]
+      public ViewResult RsvpForm()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      public ViewResult RsvpForm(GuestResponse guestResponse)
+      {
+         // TODO: Email response to the party organizer            
+         return View("Thanks", guestResponse);
+      }  
    }
 } 
