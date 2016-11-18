3f5d5ff333e9c50daa68e515e3cf2b8a4b45e5f2
jason goodell <jason.goodell@outlook.com>
2016-11-18 15:56:15 -0500

    work on retrieving data from api

--- PizzaStoreMVC2/PizzaStoreMVC2.Client/Controllers/PizzaController.cs
+++ PizzaStoreMVC2/PizzaStoreMVC2.Client/Controllers/PizzaController.cs
@@ -17,16 +17,21 @@ namespace PizzaStoreMVC2.Client.Controllers
 {
     public class PizzaController : Controller
     {
+      public static string baseurl = "http://ec2-52-23-205-25.compute-1.amazonaws.com/pizzastoreapi/api/";
+      
       static HttpClient client = new HttpClient();
       // GET: Pizza
       public ActionResult Index()
 
       {
-
-         var model = new PizzaModel();
-         var list = new List<PizzaModel>();
-         list.Add(model);
-         return View(list);
+         
+         var result = GetProductAsync("topping");
+         var t = result.Result;
+         //var model = new PizzaModel();
+         //var list = new List<PizzaModel>();
+         //list.Add(model);
+         //return View(list);
+         return View(result);
 
       }
 
@@ -41,26 +46,24 @@ namespace PizzaStoreMVC2.Client.Controllers
          return string.Format("{0} pizza with {1} crust and {2} sauce loaded with {3}  cheese and topped with {4}", model.PizzaOptions.Size, model.PizzaOptions.Crust, model.PizzaOptions.Sauce, ClientHelper.ListPrint(model.PizzaOptions.Cheeses), ClientHelper.ListPrint(model.PizzaOptions.Toppings));
 
       }
-      static async Task RunAsync()
-      {
-         
-         client.BaseAddress = new Uri("http://localhost:55268/");
-         client.DefaultRequestHeaders.Accept.Clear();
-         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-         Console.ReadLine();
-      }
+      
       static async Task<List<ToppingDTO>> GetProductAsync(string path)
       {
+
+         //client.BaseAddress = new Uri(baseurl);
+         //client.DefaultRequestHeaders.Accept.Clear();
+         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
          List<ToppingDTO> toppings = null;
          string responsestring;
-         HttpResponseMessage response = await client.GetAsync(path);
+         HttpResponseMessage response = await client.GetAsync(baseurl+path);
          if (response.IsSuccessStatusCode)
          {
             responsestring = await response.Content.ReadAsStringAsync();
-            toppings = JsonConvert.DeserializeObject<List<ToppingDTO>>(responsestring);            
+            toppings = JsonConvert.DeserializeObject<List<ToppingDTO>>(responsestring);
+                       
          }        
-         return toppings;
+         return toppings; 
       }
    }
 }
